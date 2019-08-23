bacf6b091e51f99af289ba2b82dd18a47525a652
Abdelrahman Awad <logaretm1@gmail.com>
2019-08-23 22:36:36 +0200

    chore: initial commit

---
+++ scripts/config.js
@@ -0,0 +1,121 @@
+const path = require('path');
+const fs = require('fs');
+const { rollup } = require('rollup');
+const filesize = require('filesize');
+const uglify = require('uglify-js');
+const chalk = require('chalk');
+const gzipSize = require('gzip-size');
+const typescript = require('rollup-plugin-typescript2');
+const resolve = require('rollup-plugin-node-resolve');
+const replace = require('rollup-plugin-replace');
+
+const version = process.env.VERSION || require('../package.json').version;
+
+const commons = {
+  banner: `/**
+  * vee-validate-fns v${version}
+  * (c) ${new Date().getFullYear()} Abdelrahman Awad
+  * @license MIT
+  */`,
+  outputFolder: path.join(__dirname, '..', 'dist'),
+  uglifyOptions: {
+    compress: true,
+    mangle: true
+  }
+};
+
+const paths = {
+  dist: commons.outputFolder
+};
+
+const utils = {
+  stats({ path, code }) {
+    const { size } = fs.statSync(path);
+    const gzipped = gzipSize.sync(code);
+
+    return `| Size: ${filesize(size)} | Gzip: ${filesize(gzipped)}`;
+  },
+  async writeBundle({ input, output }, fileName, minify = false) {
+    const bundle = await rollup(input);
+    const {
+      output: [{ code }]
+    } = await bundle.generate(output);
+
+    let outputPath = path.join(paths.dist, fileName);
+    fs.writeFileSync(outputPath, code);
+    let stats = this.stats({ code, path: outputPath });
+    // eslint-disable-next-line
+    console.log(`${chalk.green('Output File:')} ${fileName} ${stats}`);
+
+    if (minify) {
+      const minifiedFileName = fileName.replace('.js', '') + '.min.js';
+      outputPath = path.join(paths.dist, minifiedFileName);
+      fs.writeFileSync(outputPath, uglify.minify(code, commons.uglifyOptions).code);
+      stats = this.stats({ code, path: outputPath });
+      // eslint-disable-next-line
+      console.log(`${chalk.green('Output File:')} ${minifiedFileName} ${stats}`);
+    }
+
+    return true;
+  }
+};
+
+const builds = {
+  umd: {
+    input: 'src/index.ts',
+    format: 'umd',
+    name: 'VeeValidateFns',
+    env: 'production'
+  },
+  esm: {
+    input: 'src/index.ts',
+    format: 'es'
+  }
+};
+
+function genConfig(options) {
+  const config = {
+    input: {
+      input: options.input,
+      external: ['vue', 'vee-validate', '@vue/composition-api'],
+      plugins: [
+        typescript({ typescript: require('typescript'), useTsconfigDeclarationDir: true }),
+        replace({ __VERSION__: version }),
+        resolve()
+      ]
+    },
+    output: {
+      banner: commons.banner,
+      format: options.format,
+      name: options.name,
+      globals: {
+        vue: 'Vue',
+        'vee-validate': 'VeeValidate',
+        '@vue/composition-api': 'vueCompositionApi'
+      }
+    }
+  };
+
+  if (options.env) {
+    config.input.plugins.unshift(
+      replace({
+        'process.env.NODE_ENV': JSON.stringify(options.env)
+      })
+    );
+  }
+
+  return config;
+}
+
+const configs = Object.keys(builds).reduce((prev, key) => {
+  prev[key] = genConfig(builds[key]);
+
+  return prev;
+}, {});
+
+module.exports = {
+  configs,
+  utils,
+  uglifyOptions: commons.uglifyOptions,
+  paths
+};
